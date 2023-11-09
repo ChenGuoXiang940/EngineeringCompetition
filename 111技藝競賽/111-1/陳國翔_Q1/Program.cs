d570806802115c10f5a6771b4f55adb104959a7c
陳國翔 <chen199940@gmail.com>
2023-11-09 16:48:54 +0800

    整理

--- 111技藝競賽/111-1/陳國翔_Q1/Program.cs
+++ 111技藝競賽/111-1/陳國翔_Q1/Program.cs
@@ -12,7 +12,20 @@ namespace 陳國翔_Q1
                 /*找兩個字串最長的長度*/
                 Func<ulong, ulong, int> getmax = (x, y) => Math.Max(x.ToString().Length, y.ToString().Length);
                 /*整數轉字串左補 y 個空格*/
-                Func<ulong, int, string> pad = (x, y) => x.ToString().PadLeft(y);
+                Func<string, int, string> pad = (x, y) => x.PadLeft(y);
+                /*矩陣相乘*/
+                /*記位置
+                 a:    b:
+                 0 1   0 2 
+                 0 1   1 3
+                 2 3   0 2
+                 2 3   1 3
+                 */
+                Func<ulong[], ulong[], ulong[]> mult = (a, b) => new ulong[4] { 
+                    a[0] * b[0] + a[1] * b[2],
+                    a[0] * b[1] + a[1] * b[3],
+                    a[2] * b[0] + a[3] * b[2],
+                    a[2] * b[1] + a[3] * b[3] };
                 int n;
                 Console.Write("輸入N=");
                 if (!int.TryParse(Console.ReadLine(), out n) || n < 1 || n > 90)
@@ -27,14 +40,16 @@ namespace 陳國翔_Q1
                 {
                     if ((n & 1) == 1)
                     {
-                        ans = new ulong[4] { ans[0] * m[0] + ans[1] * m[2], ans[0] * m[1] + ans[1] * m[3], ans[2] * m[0] + ans[3] * m[2], ans[2] * m[1] + ans[3] * m[3] };
+                        ans = mult(ans, m);
                     }
                     n >>= 1;
-                    m = new ulong[4] { m[0] * m[0] + m[1] * m[2], m[0] * m[1] + m[1] * m[3], m[2] * m[0] + m[3] * m[2], m[2] * m[1] + m[3] * m[3] };
+                    m = mult(m, m);
                 }
                 int len1 = getmax(ans[0], ans[1]);
                 int len2 = getmax(ans[2], ans[3]);
-                Console.WriteLine($"[{pad(ans[0], len1)} {pad(ans[1], len2)}]\r\n[{pad(ans[2], len1)} {pad(ans[3], len2)}]");
+                List<string> res = ans.ToList().Select(item => item.ToString()).ToList();
+                Console.WriteLine($"[{pad(res[0], len1)} {pad(res[1], len2)}]\r\n" +
+                    $"[{pad(res[2], len1)} {pad(res[3], len2)}]");
             }
         }
     }
